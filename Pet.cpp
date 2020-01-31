0bb9dd96cb7cfd8c1f1fc9717daab3d6f12b49a5
root <lena1k@github.com>
2020-01-31 08:28:06 +0000

    This in my third assignment

--- Pet.cpp
+++ Pet.cpp
@@ -1,13 +1,18 @@
+// Lena Khalidi
+// Assignment 3
+
 #include <iostream>
 using namespace std;
 
 class Pet{
 public:
+  // declare variables
   string name;
   int age;
   string type;
   double weight;
 
+  // declare accessors
   string getName();
   int getAge();
   string getType();
@@ -19,45 +24,62 @@ public:
 
 };
 
+// setName mutator
 void Pet::setName(string petsName){
   name = petsName;
 }
 
+// setAge mutator
 void Pet::setAge(int petsAge){
   age = petsAge;
 }
 
+// setType mutator
 void Pet::setType(string petsType){
   type = petsType;
 }
 
+// setWeight mutator
 void Pet::setWeight(double petsWeight){
   weight = petsWeight;
 }
+
+// getName accessor
 string Pet:: getName(){
   return name;
 }
+
+// getAge accessor
 int Pet::getAge(){
   return age;
 }
+
+// getType accessor
 string Pet::getType(){
   return type;
 }
+
+// getWeight accessor
 double Pet::getWeight(){
   return weight;
 }
+
+// main class
 int main(){
+  // create object petA
   Pet petA;
   petA.setName("Alfredo");
   petA.setAge(7);
   petA.setType("Hamster");
   petA.setWeight(30);
 
+  // print results to console
   cout << "Name: " << petA.getName() << endl;
   cout << "Age: " <<  petA.getAge() << endl;
   cout << "Type: " << petA.getType() << endl;
   cout << "Weight: " << petA.getWeight() << endl;
 
+  // create object petB
   Pet petB;
   cout << endl;
   petB.setName("Chad");
